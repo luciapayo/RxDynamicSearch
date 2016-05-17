1e0dcc3dee3b50d03b6ae46311f4ab28fd0ec26a
lpay <lucia.payo@futurice.com>
2016-05-17 10:47:53 +0200

    Set content view in activity and remove unused methods related to fragments

--- app/src/main/java/com/lucilu/rxdynamicsearch/fragments/SearchFragment.java
+++ app/src/main/java/com/lucilu/rxdynamicsearch/fragments/SearchFragment.java
@@ -13,7 +13,6 @@ import com.lucilu.rxdynamicsearch.viewmodel.base.ViewModel;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,10 +33,6 @@ public final class SearchFragment extends BaseFragment {
 
     @Nullable
     private SearchView mSearchView;
-
-    public static Fragment getInstance() {
-        return new SearchFragment();
-    }
 
     @Nullable
     @Override

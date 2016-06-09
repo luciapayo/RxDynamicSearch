2edccbedbd33a4b835545fef70ccd32abed28879
Lucia Payo <lucia.payo@futurice.com>
2016-06-09 11:33:21 +0200

    Include mockito library

--- app/src/main/java/com/lucilu/rxdynamicsearch/provider/base/IJsonParserProvider.java
+++ app/src/main/java/com/lucilu/rxdynamicsearch/provider/base/IJsonParserProvider.java
@@ -14,9 +14,9 @@ public interface IJsonParserProvider {
     /**
      * Parses from
      *
-     * @param rawResourceId      the id to the raw json file in resources
-     * @param classOfType the class of the type of the objects in the list
-     * @param <T>                the type of the objects in the list
+     * @param rawResourceId the id to the raw json file in resources
+     * @param classOfType   the class of the type of the objects in the list
+     * @param <T>           the type of the objects in the list
      * @return option of the list. Can be {@link Option#NONE} if the parsing fails.
      */
     <T> Option<List<T>> parseListFromRawJsonFile(final int rawResourceId,
